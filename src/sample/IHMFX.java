a403cae0e84981525b49ac29bbbb840b770032b2
Nemense <guillaume.troesch@hotmail.fr>
2018-12-05 08:55:01 +0100

    Commit avant remplacement

--- src/sample/IHMFX.java
+++ src/sample/IHMFX.java
@@ -3,7 +3,11 @@ package sample;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.stage.Stage;
+
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
 
 public class IHMFX extends Application {
 
@@ -29,13 +33,21 @@ public class IHMFX extends Application {
         /* montage de la scene */
         MonteurScene monteurScene = new MonteurScene();
 
+        vueJeu.gridPane.getColumnConstraints().addAll(DoubleStream.of(0, 0, 0).mapToObj(width -> {
+            ColumnConstraints c = new ColumnConstraints();
+            c.setPercentWidth(width);
+            c.setFillWidth(true);
+            return c;
+        }).toArray(ColumnConstraints[]::new));
+
+        //vueJeu.gridPane.prefWidthProperty().bind(primaryStage.widthProperty().multiply(0.01));
         Scene scene = monteurScene.
                 setCentre(vueJeu.gridPane).
                 setLargeur(800).
                 setHauteur(800).
                 retourneScene();
-
         primaryStage.setScene(scene);
+
 
         primaryStage.setTitle("Sokoban");
         primaryStage.show();
--- src/sample/Vue.java
+++ src/sample/Vue.java
@@ -7,6 +7,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -25,20 +26,22 @@ public class Vue
     //public Vue(Controller controller) throws FileNotFoundException
     public Vue() throws FileNotFoundException
     {
+
+
         //** à décommenter
         // niveau = controller.getNiveau();
         //** à suppr
-        niveau = Modele.get_liste_niveaux("niveaux.xsb").get(0);
+        niveau = Modele.get_liste_niveaux("niveaux.xsb").get(1);
         //**
 
         images = new HashMap<>();
-        images.put(35, new Image(new FileInputStream("img/mur.png"),80,80,false,false));
-        images.put(36, new Image(new FileInputStream("img/caisse.png"),80,80,false,false));
-        images.put(42, new Image(new FileInputStream("img/caisseV.png"),80,80,false,false));
-        images.put(43, new Image(new FileInputStream("img/personnage_on_goal.png"),80,80,false,false));
-        images.put(64, new Image(new FileInputStream("img/personnage.png"),80,80,false,false));
-        images.put(32, new Image(new FileInputStream("img/vide.png"),80,80,false,false));
-        images.put(46, new Image(new FileInputStream("img/goal.png"),80,80,false,false));
+        images.put(35, new Image(new FileInputStream("img/mur.png"), -1, -1, false,false));
+        images.put(36, new Image(new FileInputStream("img/caisse.png"),-1,-1,false,false));
+        images.put(42, new Image(new FileInputStream("img/caisseV.png"),-1,-1,false,false));
+        images.put(43, new Image(new FileInputStream("img/personnage_on_goal.png"),-1,-1,false,false));
+        images.put(64, new Image(new FileInputStream("img/personnage.png"),-1,-1,false,false));
+        images.put(32, new Image(new FileInputStream("img/vide.png"),-1,-1,false,false));
+        images.put(46, new Image(new FileInputStream("img/goal.png"),-1,-1,false,false));
 
         listeCases = new ArrayList<>();
         for (int i = 0; i < niveau.get_level().size(); i++)
